f1c66b2ccb169a57b32fb28a8399e7013fb7ac6a
Brave Leuterio <you@example.com>
2020-11-18 11:23:50 +0800

    added html js and css for adding todo

---
+++ app.js
@@ -0,0 +1,37 @@
+//selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+//event listeners
+todoButton.addEventListener("click", addTodo);
+
+//functions
+function addTodo(event) {
+  event.preventDefault();
+
+  //todo container
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+
+  //todo text
+  const newTodo = document.createElement("li");
+  newTodo.innerText = "hey";
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+
+  //checkmark btn
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
+
+  //trash btn
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add("trash-btn");
+  todoDiv.appendChild(trashButton);
+
+  //append to list
+  todoList.appendChild(todoDiv);
+}
